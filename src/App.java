47cb43f6c78a90a5c152a8c1397ec7f5da4beecc
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-10-15 13:51:37 +0200

    fix(app): loop to delete temporary files and modify temporal directory creation

--- src/App.java
+++ src/App.java
@@ -5,7 +5,9 @@ import org.salesianos.util.FileHelper;
 import org.salesianos.util.ProcessLauncher;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
+        
         final String ROUTE_FILE = "./input/Numbers.txt";
         final String ROUTE_FINAL = "./output/file/NumbesPrime.txt";
 
@@ -20,13 +22,16 @@ public class App {
             if (!carpeta.exists()) {
                 carpeta.mkdirs();
             }
+
             Process subProcess = ProcessLauncher.initIsPrime(numbers.get(i), pathFile);
             processes.add(subProcess);
             numbersTemp.add(pathFile);
         }
+
         for (Process subProcess : processes) {
             subProcess.waitFor();
         }
+        FileHelper.mergePrimeFiles(numbersTemp, ROUTE_FINAL);
 
         for (int i = 0; i < numbers.size(); i++) {
             String pathFile = "./output/temp/" + numbers.get(i) + ".txt";
@@ -34,11 +39,9 @@ public class App {
             file.delete();
         }
 
-        FileHelper.mergePrimeFiles(numbersTemp, ROUTE_FINAL);
-
         System.out.println("There are " + FileHelper.getNumbersCount(ROUTE_FINAL) + " prime numbers.");
         FileHelper.showPrimes(ROUTE_FINAL);
 
-
     }
+
 }
